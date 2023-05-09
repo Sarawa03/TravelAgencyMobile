8deb61a5439160616e675170d9056e01db0fdee8
Sarawa03 <saraying03sb@gmail.com>
2023-05-10 00:24:58 +0200

    Imports fix

--- app/src/main/java/com/sara/travelagency/ui/view/home/HomeFragment.kt
+++ app/src/main/java/com/sara/travelagency/ui/view/home/HomeFragment.kt
@@ -1,12 +1,10 @@
 package com.sara.travelagency.ui.view.home
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import androidx.fragment.app.viewModels
@@ -22,7 +20,6 @@ class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding?=null
     private val binding get()=_binding!!
     private lateinit var autoCompleteTextView: AutoCompleteTextView
-    private lateinit var adapterItems: ArrayAdapter<String>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
