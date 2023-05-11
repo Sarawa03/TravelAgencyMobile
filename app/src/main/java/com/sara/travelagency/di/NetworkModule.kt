ff5b301b53d8828ed31fa4ea5e0e9f89bf11e4ac
Sarawa03 <saraying03sb@gmail.com>
2023-05-12 00:32:01 +0200

    todo ip public

--- app/src/main/java/com/sara/travelagency/di/NetworkModule.kt
+++ app/src/main/java/com/sara/travelagency/di/NetworkModule.kt
@@ -18,7 +18,7 @@ object NetworkModule {
     fun provideRetrofit(): Retrofit {
         return Retrofit
             .Builder()
-            .baseUrl("http://81.39.14.219:8080/travelagency/")
+            .baseUrl("http://169.254.154.183:8080/travelagency/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
